3a0ae1602d19b9a9de0f8cbf631cacae39e8588c
Farhan Amran <mfb44@cam.ac.uk>
2022-01-23 22:21:33 +0000

    minor changes

--- Task1B.py
+++ Task1B.py
@@ -2,14 +2,14 @@ from floodsystem.stationdata import build_station_list
 
 from floodsystem.geo import stations_by_distance
 
-stations = build_station_list()
+station_list = build_station_list() # Build a list of stations
 
 Cambridge_CC = (52.2053, 0.1218)
 
-station_lists = stations_by_distance(stations,Cambridge_CC)
+sorted_station_list = stations_by_distance(station_list,Cambridge_CC)
 
 print("The closest 10 stations from Cambridge City Centre are:")
-print(station_lists[:10])
+print(sorted_station_list[:10])
 
 print("The furthest 10 stations from Cambridge City Centre are:")
-print(station_lists[-10:])
+print(sorted_station_list[-10:])
