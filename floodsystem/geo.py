7e4c85de6ceb9f0defdd7952dcc8152a21ff4c43
Farhan Amran <mfb44@cam.ac.uk>
2022-01-25 23:43:52 +0000

    Task1C done!

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -13,7 +13,7 @@ from .station import MonitoringStation
 from haversine import haversine, Unit
 
 def stations_by_distance(stations,p): 
-    #This function sorts a list of MonitoringStations by their distance from a point p
+    # This function sorts a list of MonitoringStations by their distance from a point p
     output = []
     for i in range(len(stations)):
         st_name_and_town = "{} in {}".format(stations[i].name,stations[i].town)
@@ -22,6 +22,14 @@ def stations_by_distance(stations,p):
     sorted_output = sorted_by_key(output,1) 
     return(sorted_output)
 
+def stations_within_radius(stations, centre, r):
+    # This function produces a sroted list of staion names within a radius of r from a designated centre
+    output = []
+    for i in range(len(stations)):
+        if haversine(centre, stations[i].coord) <= r:
+            output.append(stations[i].name)
+    output.sort()
+    return output
 
 
 def rivers_with_station(stations):
