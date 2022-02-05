1bd48dead4bc398bed37360fdadc46dd569e17a4
Matthew Zhang <mz473@cam.ac.uk>
2022-02-05 15:52:01 +0000

    add run() function to Task 1D and 1F

--- Task1D.py
+++ Task1D.py
@@ -1,33 +1,20 @@
 import floodsystem.geo
 import floodsystem.stationdata
 
+def run():
+    # part 1: 
+    station_list = floodsystem.stationdata.build_station_list()
+    river_list = floodsystem.geo.rivers_with_station(station_list)
+    print("{} stations".format(len(river_list)))
+    print("First 10 - {}".format(river_list[0:10]))
 
-station_list = floodsystem.stationdata.build_station_list()
+    # part 2:
+    the_mapping = floodsystem.geo.stations_by_river(station_list)
+    print("Stations on River Cam are {}".format(sorted(the_mapping["River Cam"])))
+    print("Stations on River Aire are {}".format(sorted(the_mapping["River Aire"])))
+    print("Stations on River Thames are {}".format(sorted(the_mapping["River Thames"])))
 
-# part 1: 
-river_list = floodsystem.geo.rivers_with_station(station_list)
 
-print("{} stations".format(len(river_list)))
-
-print("First 10 - {}".format(river_list[0:10]))
-
-# part 2:
-the_mapping = floodsystem.geo.stations_by_river(station_list)
-print(sorted(the_mapping["River Cam"]))
-print((sorted(the_mapping["River Aire"])))
-print(sorted(the_mapping["River Thames"]))
-
-"""
-I found a problem here, the number of stations with River Aire
-is different from the representative result
-I guess the data is prob different now
-
-e.g. there are 21 stations in the answer
-but 24 in my codes
-
-From answer:
-['Airmyn', 'Apperley Bridge', 'Armley', 'Beal Weir Bridge', 'Bingley', 'Birkin Holme Washlands', 'Carlton Bridge', 'Castleford', 'Chapel Haddlesey', 'Cononley', 'Cottingley Bridge', 'Ferrybridge Lock', 'Fleet Weir', 'Gargrave', 'Kildwick', 'Kirkstall Abbey', 'Knottingley Lock', 'Leeds Crown Point', 'Saltaire', 'Snaygill', 'Stockbridge']
-
-From new calculation:
-['Airmyn', 'Apperley Bridge', 'Armley', 'Beal Weir Bridge', 'Bingley', 'Birkin Holme Washlands', 'Carlton Bridge', 'Castleford', 'Chapel Haddlesey', 'Cononley', 'Cottingley Bridge', 'Ferrybridge Lock', 'Fleet Weir', 'Gargrave', 'Kildwick', 'Kirkstall Abbey', 'Knottingley Lock', 'Leeds Crown Point', 'Leeds Crown Point Flood Alleviation Scheme', 'Leeds Knostrop Weir Flood Alleviation Scheme', 'Oulton Lemonroyd', 'Saltaire', 'Snaygill', 'Stockbridge']
-"""
+if __name__ == "__main__":
+    print("*** Task 1D: CUED Part IA Flood Warning System ***")
+    run()
--- Task1E.py
+++ Task1E.py
@@ -1,9 +1,14 @@
 import floodsystem.geo
 import floodsystem.stationdata
 
+def run():
 
-station_list = floodsystem.stationdata.build_station_list()
+    station_list = floodsystem.stationdata.build_station_list()
 
-the_result = floodsystem.geo.rivers_by_station_number(station_list,9)
+    the_result = floodsystem.geo.rivers_by_station_number(station_list,9)
 
-print(the_result)
+    print("The first 9th stations are {}".format(the_result))
+
+if __name__ == "__main__":
+    print("*** Task 1E: CUED Part IA Flood Warning System ***")
+    run()
