e5343bde37b1596da438ea0b43fe8f2e4cc12496
Matthew Zhang <mz473@cam.ac.uk>
2022-02-18 23:39:00 +0000

    Task 2C done

---
+++ Task2C.py
@@ -0,0 +1,19 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+
+from floodsystem.flood import stations_highest_rel_level
+
+def run():
+    # Build list of stations
+    station_list = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(station_list)
+
+    output_list = stations_highest_rel_level(station_list,10)
+
+    for element in output_list:
+        print(element[0].name, element[1])
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -3,7 +3,9 @@ from . import utils
 from .station import MonitoringStation
 
 def stations_level_over_threshold(stations, tol):
-    # This function returns a list of tuples, where each tuple holds a station at which the latest relative water level is over tol and the relative water level at the station
+    # This function returns a list of tuples, 
+    # where each tuple holds a station at which the latest relative water level 
+    # is over tol and the relative water level at the station
     # (Task 2B)
     output = []
     for station in stations:
@@ -13,3 +15,10 @@ def stations_level_over_threshold(stations, tol):
                 output.append(st_id)
     sorted_output = utils.sorted_by_key(output,1,reverse=True) 
     return sorted_output
+    
+def stations_highest_rel_level(stations, N):
+    # (Task 2C)
+    # output is a list containing the Nth stations with highest reletive water level
+
+    # Set the tolerance to minus infinity and return the first N elements
+    return stations_level_over_threshold(stations,tol = float('-inf'))[0:N]
